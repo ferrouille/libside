b543997418994bfcfa322b1e095af5e6f94d0ebc
ferrouille <git@ferrouille.nl>
2022-01-18 19:34:12 +0100

    Add wrapper for php7.4-fpm

--- libside/src/builder/php_fpm.rs
+++ libside/src/builder/php_fpm.rs
@@ -32,3 +32,31 @@ impl AptPackage<"php8.0-fpm"> {
         ]
     }
 }
+
+impl AptPackage<"php7.4-fpm"> {
+    pub fn binary(&self) -> Path<FromPackage> {
+        Path {
+            base: PathBuf::from("/usr/sbin/php-fpm7.4"),
+            path: PathBuf::new(),
+            loc: FromPackage,
+            node: Some(self.graph_node()),
+        }
+    }
+
+    pub fn default_service_files(&self) -> Vec<Path<FromPackage>> {
+        vec![
+            Path {
+                base: PathBuf::from("/lib/systemd/system/php7.4-fpm.service"),
+                path: PathBuf::new(),
+                loc: FromPackage,
+                node: Some(self.graph_node()),
+            },
+            Path {
+                base: PathBuf::from("/etc/init.d/php7.4-fpm"),
+                path: PathBuf::new(),
+                loc: FromPackage,
+                node: Some(self.graph_node()),
+            },
+        ]
+    }
+}
